7d8eb26bb239cd520ae0c3854f109352bd2f4e00
Huelva91 <huelva91gmail.com>
2022-05-18 11:55:43 +0200

    Firts component

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Testimonio() {
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        src={require("../imagenes/testimonio-emma.png")}
+        alt="Foto de Emma"
+        className="imagen-testimonio"
+      />
+    </div>
+  );
+}
